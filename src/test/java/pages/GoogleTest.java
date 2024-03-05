7c165b2005d6f78d84c044b7b84cf957110e85cb
edwinchi <chipineiro@gmail.com>
2024-03-04 20:52:20 -0600

    Create GoogleTest.java

---
+++ src/test/java/pages/GoogleTest.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void goToGoogle(){
+        driver.get("https://www.google.com");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        if (driver != null){
+            driver.quit();
+        }
+    }
+}
