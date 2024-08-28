31a72d42601d6219574ae9d57f02f6cedf58c09a
chipi <chipineiro@gmail.com>
2024-08-27 22:37:16 -0600

    Add cucumber json report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,18 +5,17 @@ import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.testng.annotations.AfterTest;
 import pages.BasePage;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources", 
     glue="steps",
-    plugin={"pretty", "html:target/cucumber-reports.html"},
+    plugin={"pretty", "json:target/cucumber-reports.json"},
     tags = "@Test")
 
 public class TestRunner {
 
-    @AfterAll
+    @AfterClass
     public static void cleanDriver(){
         BasePage.closeBrowser();
     } 
